a79d32be733a72805354953b5ee77134fb8f63ce
Harshua <86357941+Harshua@users.noreply.github.com>
2021-08-02 11:53:01 +0000

    Update w7.sh

--- w7.sh
+++ w7.sh
@@ -4,7 +4,6 @@
 apt install unzip -y
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok-stable-linux-amd64.zip
 clear
-echo "Windows 7"
 read -p "Paste authtoken here (Copy and Right-click to paste): " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp --region eu 30889 &>/dev/null &
